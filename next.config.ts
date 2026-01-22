c22966e0853ff1d3e50d2d85e4fba65c266caff1
sumann <sumannnarya6969@gmail.com>
2026-01-23 00:16:13 +0530

    upload limit fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '20mb',
+    },
+  },
 
-export default nextConfig;
+  export default nextConfig;
